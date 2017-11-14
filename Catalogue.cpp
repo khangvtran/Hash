1cc306a853878c6492d4edc665b15cb2f98d7889
khangvtran <vinhkhangtran09@gmail.com>
2017-11-13 18:56:38 -0800

    Catalogue.cpp is built up to case 3

--- Catalogue.cpp
+++ Catalogue.cpp
@@ -45,6 +45,7 @@ int main()
 		getline(fin, buf);
 		isbn = stoi(buf);
 		getline(fin, buf);
+
 		Book book(title, author, price, isbn);
 		ht.insert(book);
 		//cout << book << endl;
@@ -52,11 +53,53 @@ int main()
 
 	// prompt user
 	cout << "Welcome to the Best Reads App!" << endl;
+
 	int userInput = 0;
+	int groupNumber = 0;
 	while (userInput != 6)
 	{
 		promptUser();
 		cin >> userInput;
+		cin.ignore();
+		switch(userInput)
+		{
+		case 1:
+			ht.printTable(cout);
+			break;
+		case 2:
+			cout << "Enter the group number (1-10): ";
+			cin >> groupNumber;
+			cin.ignore();  //or cin >> ws
+			cout << endl;
+			cout << "The books in this group are:" << endl << endl;
+			ht.printBucket(cout, groupNumber - 1);
+			break;
+		case 3:
+		{
+			cout << "Enter the title: ";
+			getline(cin, title);
+			cout << "Enter the author: ";
+			getline(cin, author);
+			cout << "Enter the price: ";
+			getline(cin, buf);
+			price = stod(buf);
+			cout << "Enter the isbn: ";
+			getline(cin, buf);
+			isbn = stoi(buf);
+
+			Book newBook(title, author, price, isbn);
+			ht.insert(newBook);
+			cout << title << " has been added to the catalogue" << endl << endl;
+			break;
+		}
+		case 4:
+			cout << "123" << endl;
+			break;
+		case 5:
+			cout << "456" << endl;
+			break;
+
+		}
 
 	}
 
